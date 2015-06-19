96c8ba13174d25218d348044fc46365b9d878dfb
adriaade <adriaade@gmail.com>
2015-06-19 13:16:47 +0200

    a

---
+++ prov - copia.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World --- jjjj');
+});
+
+app.listen(8000);
+
+console.log("escuchando port 8000");
+
